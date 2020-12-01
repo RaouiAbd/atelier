c28e26428787e58f6bf2b02df53134120dc58449
scrumMasterFirstName scrumMasterLastName <abdelazizraoui3@gmail.com>
2020-12-01 19:51:16 +0000

    test

--- src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
@@ -3,7 +3,7 @@ package emi.iql.eurekaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//test
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication {
