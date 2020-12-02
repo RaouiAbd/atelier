b713c993dc6e930d7b214c039bce684dfab95fb6
Youness TIZI <younesstizi@gmail.com>
2020-12-02 11:13:18 +0100

    ..

--- src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
@@ -13,6 +13,6 @@ public class EurekaServiceApplication {
         SpringApplication.run(EurekaServiceApplication.class, args);
     }
     
-    public void abd(String nam) {}
+    public void ab(String nam) {}
 
 }
