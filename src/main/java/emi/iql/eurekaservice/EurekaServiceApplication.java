6079d795fd1b6c120b5cbaeff1fefefc17b998e7
RaouiAbd <abdelazizraoui3@gmail.com>
2020-12-11 18:59:04 +0000

    rebase

--- src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/emi/iql/eurekaservice/EurekaServiceApplication.java
@@ -3,11 +3,7 @@ package emi.iql.eurekaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-<<<<<<< HEAD
-//test of merge3
-=======
-//test
->>>>>>> d6603dd16bd55c53ee06fff8710226bda7e4670f
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication {
@@ -17,9 +13,5 @@ public class EurekaServiceApplication {
         SpringApplication.run(EurekaServiceApplication.class, args);
     }
     
-<<<<<<< HEAD
-=======
-    public void tii(String nam) {}
->>>>>>> d6603dd16bd55c53ee06fff8710226bda7e4670f
 
 }
